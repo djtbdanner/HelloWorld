58fe701d2aa85d8517f57fe1e3d632c71a7b3416
Dave Danner <djtbdanner@yahoo.com>
2016-08-20 08:20:13 -0500

    working

--- src/main/java/com/example/HelloWorldController.java
+++ src/main/java/com/example/HelloWorldController.java
@@ -1,13 +1,20 @@
 package com.example;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-    @RequestMapping("/")
-    public String greeting(@RequestParam(value="name", defaultValue="World") String name) {
-    	return " Hello " + name;
-    }
+
+	Log log = LogFactory.getLog(this.getClass());
+
+	@RequestMapping("/")
+	public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+
+		log.info("Saying hello to " + (name == null ? "World" : name));
+		return " Hello " + name;
+	}
 }
--- src/main/java/com/example/HelloworldApplication.java
+++ src/main/java/com/example/HelloworldApplication.java
@@ -2,11 +2,24 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HelloworldApplication {
+public class HelloworldApplication  extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloworldApplication.class, args);
 	}
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    	
+    	System.out.println("*****\n****\n****\nIT started\n*****\n****\n****\n");
+        return application.sources(HelloworldApplication.class);
+    }
+
+
 }
+
+
